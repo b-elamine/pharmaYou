fa8a7300c95fb5902580a8ba3a525936d55a2d95
b-elamine <m.bechorfa@esi-sba.dz>
2021-04-16 04:40:04 +0200

    Create new empty cards

--- src/views/dashboard/analytics/AnalyticsDashboard.js
+++ src/views/dashboard/analytics/AnalyticsDashboard.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Row, Col } from "reactstrap"
+import ApexColumnCharts from "./ApexColumnCharts"
 // import SalesCard from "./SalesCard"
 // import SuberscribersGained from "../../ui-elements/cards/statistics/SubscriberGained"
 // import OrdersReceived from "../../ui-elements/cards/statistics/OrdersReceived"
@@ -11,7 +12,6 @@ import { Row, Col } from "reactstrap"
 // import DispatchedOrders from "./DispatchedOrders"
 import DataTableCustom from "./DataTableCustom"
 import "../../../assets/scss/pages/dashboard-analytics.scss"
-
 // let $primary = "#7367F0",
 //   $danger = "#EA5455",
 //   $warning = "#FF9F43",
@@ -28,51 +28,59 @@ class AnalyticsDashboard extends React.Component {
   render() {
     return (
       <React.Fragment>
-        
-        <Row className="match-height">
-          <Col lg="6" md="12">
-            {/* <SalesCard /> */}
-          </Col>
-          <Col lg="3" md="6" sm="12">
-            {/* <SuberscribersGained /> */}
-          </Col>
-          <Col lg="3" md="6" sm="12">
-            {/* <OrdersReceived /> */}
+        {/* <Row className="match-height">
+        <Col xl="3" lg="4" sm="">
+            <h5>Particuliers</h5>
+            <Particulier
+              hideChart
+              statTitle="Ordonnances En attente"
+            />
           </Col>
         </Row>
         <Row className="match-height">
-          <Col md="6" sm="12">
-            {/* <AvgSession labelColor={$label_color} primary={$primary} /> */}
+        <Col xl="3" lg="4" sm="">
+            <h5>Profesionnels</h5>
+            <Profesionnels
+              hideChart
+              statTitle="Ordonnances En attente"
+            />
           </Col>
-          <Col md="6" sm="12">
-            {/* <SupportTracker
-              primary={$primary}
-              danger={$danger}
-              white={$white}
-            /> */}
+        </Row> */}
+        <Row>
+          <Col sm="12">
+          <h5>Particuliers</h5>
+            <div 
+            style={{
+            backgroundImage:"linear-gradient(#F1AE73 , #EEE4DC)",
+            borderRadius:"15px",
+            width:"100%",
+            marginRight:"1rem",
+            height:"12rem",
+            textAlign:"center",
+            paddingTop:"5rem",
+            marginBottom:"35px",
+            }}>
+              {/* <ApexColumnCharts/> */}
+            </div>
           </Col>
         </Row>
-        <Row className="match-height">
-          <Col lg="4">
-            {/* <ProductOrders
-              primary={$primary}
-              warning={$warning}
-              danger={$danger}
-              primaryLight={$primary_light}
-              warningLight={$warning_light}
-              dangerLight={$danger_light}
-            /> */}
-          </Col>
-          <Col lg="4">
-            {/* <SalesStat
-              strokeColor={$stroke_color}
-              infoLight={$info_light}
-              primary={$primary}
-              info={$info}
-            /> */}
-          </Col>
-          <Col lg="4">
-            {/* <ActivityTimeline /> */}
+        <Row>
+          <Col sm="12">
+          <h5>Profesionnels</h5>
+            <div 
+            style={{
+            backgroundImage:"linear-gradient(#0C68FA , #00CFE8)",
+            borderRadius:"15px",
+            width:"100%",
+            marginRight:"1rem",
+            height:"12rem",
+            textAlign:"center",
+            paddingTop:"5rem",
+            marginBottom:"35px",
+            
+            }}>
+              
+            </div>
           </Col>
         </Row>
         <Row>
---
+++ src/views/dashboard/analytics/Profesionnels.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+import Chart from "react-apexcharts";
+
+class Profesionnels extends React.Component {
+  render() {
+    return (
+      <Card
+        style={{
+          borderRadius: "28px",
+          width: "98rem",
+          height:"12rem",
+          backgroundColor:"#069DF1"
+        }}
+      >
+        <CardBody          
+          className={`${
+            this.props.className ? this.props.className : "stats-card-body"
+          } d-flex ${
+            !this.props.iconRight && !this.props.hideChart
+              ? "flex-column align-items-start"
+              : this.props.iconRight
+              ? "justify-content-between flex-row-reverse align-items-center"
+              : this.props.hideChart && !this.props.iconRight
+              ? "justify-content-center flex-column text-center"
+              : null
+          } ${!this.props.hideChart ? "pb-0" : "pb-2"}  pt-1 ${
+            this.props.bg_color ? "gradient-light-" + this.props.bg_color : ''
+          }`}
+        >
+          <div className="title-section">
+            <h2 className="text-bold-600 mt-1 mb-25">{this.props.stat}</h2>
+            <p className="mb-0 p-0">{this.props.statTitle}</p>
+          </div>
+        </CardBody>
+        {!this.props.hideChart && (
+          <Chart
+            options={this.props.options}
+            series={this.props.series}
+            type={this.props.type}
+            height={this.props.height ? this.props.height : 100}
+          />
+        )}
+      </Card>
+    );
+  }
+}
+export default Profesionnels;
